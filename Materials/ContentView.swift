8dc4dcc24707187b7778ee58ca68d798ba1feb77
Philip Davis <philipcdavis11@gmail.com>
2021-08-24 20:51:20 -0700

    removed call to UIViewRepresentable

--- Materials/ContentView.swift
+++ Materials/ContentView.swift
@@ -7,37 +7,23 @@
 
 import SwiftUI
 
-struct Blur: UIViewRepresentable {
-    var style: UIBlurEffect.Style = .systemUltraThinMaterial
-    
-    func makeUIView(context: Context) -> UIVisualEffectView {
-        return UIVisualEffectView(effect: UIBlurEffect(style: style))
-    }
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
-        uiView.effect = UIBlurEffect(style: style)
-    }
-}
-
-
 struct ContentView: View {
-    private var materials: [UIBlurEffect.Style] = [ .systemUltraThinMaterial, .systemThinMaterial, .systemMaterial, .systemThickMaterial, .systemChromeMaterial]
-    private var materialLabels: [String] = [ "System Ultra Thin Material", "System Thin Material", "System Material", "System Thick Material", "System Chrome Material"]
+    private var materials: [Material] = [.ultraThinMaterial, .thinMaterial, .regularMaterial, .thickMaterial, .ultraThickMaterial]
+    private var materialLabels: [String] = ["Ultra Thin Material", "Thin Material", "Regular Material", "Thick Material", "Ultra Thick Material"]
     
     var body: some View {
         ZStack {
             Image("background")
                 .resizable()
-                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/ .fill/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/ .all/*@END_MENU_TOKEN@*/)
             TabView {
-                ForEach(0..<materials.count) { i in
+                ForEach(0 ..< materials.count) { i in
                     ZStack {
                         Color(.clear)
-                        Text(materialLabels[i]).padding().opacity(0.7)
+                        Text(materialLabels[i]).padding().foregroundColor(.secondary)
                         
-                        
-                    }.background(Blur(style: materials[i])).cornerRadius(24)
-                    
+                    }.background(materials[i]).cornerRadius(24)
                 }
                 .padding(.all, 10)
             }
@@ -50,7 +36,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
-        
     }
 }
